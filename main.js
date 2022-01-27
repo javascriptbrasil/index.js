c2ee52394f1109a55c23e24b258f88e287e1b224
Andreribas15 <54609210+Andreribas15@users.noreply.github.com>
2022-01-27 16:11:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 document.writeln("<h1>Website under development...</h1>");
-document.writeln("<p>If you want to help with the development of this site, go to GitHub.</p>");
+document.writeln("<p>If you want to help with the development of this site, go to <a href='https://github.com/javascriptbrasil'>GitHub</a>.</p>");
